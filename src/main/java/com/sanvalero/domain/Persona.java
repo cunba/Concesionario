9b67f4130fc8489616a27af55c40aa129df66d38
Irene <ire.cunba@gmail.com>
2021-05-23 19:53:40 +0200

    Crear clase persona

---
+++ src/main/java/com/sanvalero/domain/Persona.java
@@ -0,0 +1,75 @@
+package com.sanvalero.domain;
+
+import java.util.Objects;
+
+public class Persona {
+    private final String id;
+    private final String nombre;
+    private final String apellido;
+    private final String dni;
+    private String telefono;
+    private String email;
+
+    public Persona(String id, String nombre, String apellido, String dni) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Persona)) return false;
+        Persona persona = (Persona) o;
+        return getId().equals(persona.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Persona {" +
+                "ID = '" + id + '\'' +
+                ", nombre = '" + nombre + '\'' +
+                ", apellido = '" + apellido + '\'' +
+                ", dni = '" + dni + '\'' +
+                ", telefono = '" + telefono + '\'' +
+                ", email = '" + email + '\'';
+    }
+}
