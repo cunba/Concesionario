3c1b96ee1e06e62c38576d479eb61cf37a617943
Irene <ire.cunba@gmail.com>
2021-05-23 21:08:12 +0200

    creación clase venta

---
+++ src/main/java/com/sanvalero/domain/Venta.java
@@ -0,0 +1,63 @@
+package com.sanvalero.domain;
+
+import java.util.Objects;
+
+public class Venta {
+    private final String id;
+    private final float importeVenta;
+    private final String idVendedor;
+    private final String idComprador;
+    private final String matricula;
+
+    public Venta(String id, float importeVenta, String idVendedor, String idComprador, String matricula) {
+        this.id = id;
+        this.importeVenta = importeVenta;
+        this.idVendedor = idVendedor;
+        this.idComprador = idComprador;
+        this.matricula = matricula;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public float getImporteVenta() {
+        return importeVenta;
+    }
+
+    public String getIdVendedor() {
+        return idVendedor;
+    }
+
+    public String getIdComprador() {
+        return idComprador;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Venta)) return false;
+        Venta venta = (Venta) o;
+        return getId().equals(venta.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Venta {" +
+                "ID = '" + id + '\'' +
+                ", importeVenta = '" + importeVenta + '\'' +
+                ", idVendedor = '" + idVendedor + '\'' +
+                ", idComprador = '" + idComprador + '\'' +
+                ", matricula = '" + matricula + '\'' +
+                '}';
+    }
+}
