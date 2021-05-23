82284c698ae1dca6f40cb8318fbf1c2918e386f1
Irene <ire.cunba@gmail.com>
2021-05-23 20:01:42 +0200

    Crear clase usuario

---
+++ src/main/java/com/sanvalero/domain/Usuario.java
@@ -0,0 +1,15 @@
+package com.sanvalero.domain;
+
+public class Usuario extends Persona {
+    public Usuario(String id, String nombre, String apellido, String dni) {
+        super(id, nombre, apellido, dni);
+    }
+
+    private void consultarCoches() {
+
+    }
+
+    private void consultarMotos() {
+
+    }
+}
