4340f8c69f7d7e39afd05afd7964c747d375b004
Irene <ire.cunba@gmail.com>
2021-05-24 22:02:42 +0200

    -modificacion metodos administrador por ArrayList

--- src/main/java/com/sanvalero/domain/Administrador.java
+++ src/main/java/com/sanvalero/domain/Administrador.java
@@ -3,37 +3,28 @@ package com.sanvalero.domain;
 import java.util.ArrayList;
 
 public class Administrador extends Persona {
-    private final ArrayList<Vendedor> vendedores;
-    private final ArrayList<Comprador> compradores;
-    private final ArrayList<Administrador> administradores;
-    private final ArrayList<Coche> coches;
-    private final ArrayList<Moto> motos;
     private int idVendedor;
     private int idComprador;
     private int idAdministrador;
 
     public Administrador(int id, String nombre, String apellido, String dni, String telefono, String email) {
         super(id, nombre, apellido, dni, telefono, email);
-        vendedores = new ArrayList<>();
-        compradores = new ArrayList<>();
-        administradores = new ArrayList<>();
-        coches = new ArrayList<>();
-        motos = new ArrayList<>();
         idVendedor = 0;
         idComprador = 0;
         idAdministrador = 0;
     }
 
-    public void registrarVendedor(String nombre, String apellido, String dni, String telefono, String email, float sueldo) {
+    public ArrayList<Vendedor> registrarVendedor(String nombre, String apellido, String dni, String telefono, String email, float sueldo, ArrayList<Vendedor> vendedores) {
         idVendedor = vendedores.size();
 
         Vendedor vendedor = new Vendedor(idVendedor, nombre, apellido, dni, telefono, email, sueldo);
         vendedor.setCochesVendidos(0);
 
         vendedores.add(vendedor);
+        return vendedores;
     }
 
-    public void registrarComprador(String nombre, String apellido, String dni, String telefono, String email) {
+    public ArrayList<Comprador> registrarComprador(String nombre, String apellido, String dni, String telefono, String email, ArrayList<Comprador> compradores) {
         idComprador = compradores.size();
 
         Comprador comprador = new Comprador(idComprador, nombre, apellido, dni, telefono, email);
@@ -41,37 +32,29 @@ public class Administrador extends Persona {
         comprador.setCochesComprados(0);
 
         compradores.add(comprador);
+        return compradores;
     }
 
-    public void registrarAdministrador(String nombre, String apellido, String dni, String telefono, String email) {
+    public ArrayList<Administrador> registrarAdministrador(String nombre, String apellido, String dni, String telefono, String email, ArrayList<Administrador> administradores) {
         idAdministrador = administradores.size();
 
         Administrador administrador = new Administrador(idAdministrador, nombre, apellido, dni, telefono, email);
 
         administradores.add(administrador);
+        return administradores;
     }
 
-    public void registrarCoche(String matricula, String marca, String modelo, String tipo, String tipoCombustible, int caballos, int nPuertas, int nAsientos) {
+    public ArrayList<Coche> registrarCoche(String matricula, String marca, String modelo, String tipo, String tipoCombustible, int caballos, int nPuertas, int nAsientos, ArrayList<Coche> coches) {
         Coche coche = new Coche(matricula, marca, modelo, tipo, tipoCombustible, caballos, nPuertas, nAsientos);
 
         coches.add(coche);
-    }
-
-    public void registrarMoto(String matricula, String marca, String modelo, String tipo, String tipoCombustible, int caballos) {
-        Moto moto = new Moto(matricula, marca, modelo, tipo, tipoCombustible, caballos);
-
-        motos.add(moto);
-    }
-
-    public ArrayList<Comprador> mostrarCompradores() {
-        return compradores;
-    }
-
-    public ArrayList<Coche> mostrarCoches() {
         return coches;
     }
 
-    public ArrayList<Moto> mostrarMotos() {
+    public ArrayList<Moto> registrarMoto(String matricula, String marca, String modelo, String tipo, String tipoCombustible, int caballos, ArrayList<Moto> motos) {
+        Moto moto = new Moto(matricula, marca, modelo, tipo, tipoCombustible, caballos);
+
+        motos.add(moto);
         return motos;
     }
 
--- src/main/java/com/sanvalero/domain/Usuario.java
+++ src/main/java/com/sanvalero/domain/Usuario.java
@@ -1,5 +1,7 @@
 package com.sanvalero.domain;
 
+import java.util.ArrayList;
+
 public class Usuario extends Persona {
     public Usuario(int id, String nombre, String apellido, String dni, String telefono, String email) {
         super(id, nombre, apellido, dni, telefono, email);
