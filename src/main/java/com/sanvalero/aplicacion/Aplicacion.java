328527ae66fb3e60c766441b612e714565d649fd
Irene <ire.cunba@gmail.com>
2021-05-24 16:25:41 +0200

    creación clase aplicación

---
+++ src/main/java/com/sanvalero/aplicacion/Aplicacion.java
@@ -0,0 +1,8 @@
+package com.sanvalero.aplicacion;
+
+public class Aplicacion {
+    public static void main(String[] args) {
+        Concesionario concesionario = new Concesionario();
+        concesionario.menuInicial();
+    }
+}
